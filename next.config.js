c1e0f7c963f32e4ebc70a59f524d51c0e61c5678
jeffreystorer <jeffrey.storer@gmail.com>
2023-06-30 17:33:21 -0400

    6/30: cleanup config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withFonts = require('next-fonts');
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['drive.google.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  enableSvg: true,
+  
   webpack(config, options) {
     return config;
   },
